4b679c7fef959958e5abe556b2646d8079303192
bcastor <65585051+bcastor@users.noreply.github.com>
2020-06-18 16:03:43 -0700

    added a function to read user input and a function to tokenize the input

--- main.c
+++ main.c
@@ -1,11 +1,64 @@
-//
-// Created by Brandon Castor on 6/18/20.
-//
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define TOKEN_BUFFSIZE 64
+#define TOKEN_DELIM " \n"
+
+
+/**
+ * reads user input and returns it as output
+ * @return line
+ */
+char *msh_read(void){
+
+    char *line = NULL;
+    size_t buffsize = 0;
+
+    if(getline(&line, &buffsize, stdin) == 0){
+        if(feof(stdin)){
+            exit(EXIT_SUCCESS);//end of line
+        }
+
+        else{
+            perror(">> Input error!\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return line;
+}
+/**
+ * tokenizes a line by a delimiter and splits the line
+ * by a delimiter and converts it to tokens
+ *
+ * @param line
+ * @return tokens
+ */
+char **msh_tokenize_line(char *line){
+
+    int buff = TOKEN_BUFFSIZE;
+    int i = 0;
+    //allocate memory for tokens
+    char **tokens = malloc(buff * sizeof(char*));
+    char *token;
+    //tokenize by delimiter
+    token = strtok(line, TOKEN_DELIM);
+    //move each token into tokens
+    while (token != NULL){
+        tokens[i] = token;
+        i++;
+        token = strtok(NULL, TOKEN_DELIM);
+    }
+
+    tokens[i] = NULL;
+    return tokens;
+}
 
 int main(){
 
-    printf("hello world");
+
 
     return 0;
 }
